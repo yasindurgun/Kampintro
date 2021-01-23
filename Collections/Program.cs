1798dde507d1c8fd52916a0c7ce2ef85363c6660
Yasin Durgun <yasin.durgun52@gmail.com>
2021-01-23 19:37:27 +0300

    OOP and Collections

---
+++ Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Yasin", "Ertaş" };
+            // isimler.Add("Yasin");
+            // isimler.Add("Ertaş");
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            isimler.Add("Engin");
+            Console.WriteLine(isimler[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
